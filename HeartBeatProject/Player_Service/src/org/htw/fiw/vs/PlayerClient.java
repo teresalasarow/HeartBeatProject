1574a6482ee3f6e93ffc7d1b1508a335944bcdf0
Teresa Lasarow <s0554735@htw-berlin.de>
2017-12-19 10:09:23 +0100

    Implementierung Player-Service

---
+++ HeartBeatProject/Player_Service/src/org/htw/fiw/vs/PlayerClient.java
@@ -0,0 +1,39 @@
+package src.org.htw.fiw.vs;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.ExportException;
+
+public class PlayerClient implements Remote {
+	
+	public static void main(String[] args) throws AlreadyBoundException {
+		Registry registry = null;
+		try {
+			PlayerImpl player = new PlayerImpl();
+			try {
+				//creates Registry if not there
+				registry = LocateRegistry.createRegistry(8080);
+				registry.bind("Player", player);
+				System.out.println("created 8080 Registry");
+				
+			} catch (ExportException e) {
+				e.printStackTrace();
+				
+			}
+			//gets Registry which already exists or which was just created from HeartBeatClient
+			registry = LocateRegistry.getRegistry(8080);
+			registry.list();
+			registry.rebind("Player", player);
+			System.out.println("located 8080 Registry");
+			player.turnVolumeDownTo(10);
+			
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ HeartBeatProject/Player_Service/src/org/htw/fiw/vs/PlayerImpl.java
@@ -0,0 +1,71 @@
+package src.org.htw.fiw.vs;
+
+import java.rmi.RemoteException;
+
+import javax.print.attribute.standard.Media;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import rmi.interfaces.IPlayer;
+//import javafx.scene.media.Media;
+//import javafx.scene.media.MediaPlayer;
+import java.io.*;
+import sun.audio.*;
+
+public class PlayerImpl extends java.rmi.server.UnicastRemoteObject implements IPlayer {
+
+	private static final long serialVersionUID = 1L;
+
+	protected PlayerImpl() throws RemoteException {
+		super();
+	}
+
+	@Override
+	public void turnVolumeDownTo(int volume) throws RemoteException {
+		/*String bip = "bip.mp3";
+		Media hit = new Media(new File(bip).toURI().toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(hit);
+		mediaPlayer.play();*/
+		
+		{
+			//creates AudioInputStream
+		    AudioInputStream audioInputStream = null;
+		    Clip clip = null;
+			try {
+				//creates AudioInputStream
+				audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/Shared/Reallusion/Template/CrazyTalk Animator 3 Template/Sound/Partners In Rhyme/Sparrow.WAV"));
+			} catch (UnsupportedAudioFileException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+				try {
+					clip = AudioSystem.getClip();
+				} catch (LineUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		    try {
+				clip.open(audioInputStream);
+			} catch (LineUnavailableException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    //gets control for Volume change
+		    FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+		    // Reduce volume by 10 decibels
+		    gainControl.setValue(-10.0f); 
+		    clip.start();
+		  }
+	
+	}
+
+	@Override
+	public void killMusic() throws RemoteException {
+
+	}
+
+}
